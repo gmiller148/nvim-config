5e5c98f14b7d9b30e7ec0cbfcd9c9f47e17e8871
Grant Miller <gmmiller@mit.edu>
2024-06-04 11:27:34 -0500

    Made the LSP actually work

--- lua/gmiller/lazy/treesitter.lua
+++ lua/gmiller/lazy/treesitter.lua
@@ -19,16 +19,22 @@ return {
             "vim",
             "vimdoc",
             "rust",
+            "toml",
         },
         indent = {
             enable = true,
+        },
+        rainbow = {
+            enable = true,
+            extended_mode = true,
+            max_file_lines = nil,
         },
     },
     config = function(_, opts)
         -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
         -- Prefer git instead of curl in order to improve connectivity in some environments
         require("nvim-treesitter.install").prefer_git = true
-        ---@diagnostic disable-next-line: missing-fields
+        -- @diagnostic disable-next-line: missing-fields
         require("nvim-treesitter.configs").setup(opts)
     end,
 }
--- lua/gmiller/remap.lua
+++ lua/gmiller/remap.lua
@@ -4,18 +4,21 @@ vim.keymap.set("n", "<leader>pv", "<cmd>Ex<CR>", { noremap = true, silent = true
 
 vim.keymap.set("n", "J", "mzJ`z")
 
+-- > get the end line of the highlighted section
+-- :m ... +1 move the selection down one line
+-- <CR> execute the command
+-- gv highlight the previously highlighted section
+-- = handle indentation according to the treesitter/lsp
+-- rehighlight
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
 
 vim.keymap.set("n", "<leader>w", "<cmd>update<CR>")
 vim.keymap.set("n", "<leader>w", "<cmd>update<CR>")
-
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
